1b57e00ff4b8fd3db28ab864c6e0821cb966303d
lichlp <hao.lip@husky.neu.edu>
2020-06-25 16:24:39 -0400

    moviecard and css

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,8 +1,23 @@
 import React from 'react';
+import './MovieCard.css';
 
-const MovieCard = () => {
+const imgPrefix = 'https://image.tmdb.org/t/p/w500/';
+
+const MovieCard = props => {
+    const imgSrc = imgPrefix + props.poster_path;
     return (
-        true;
+        <div className='moviecard'>
+            <img className='poster' src= {imgSrc} alt='none'/>
+            <div className='title'>{props.title}</div>
+            <div className='release_date'>release_date:{props.release_date}</div>
+            <span className='vote_average'>vote_average:{props.vote_average}____</span>
+            <span className='vote_count'>vote_count:{props.vote_count}</span>
+            <br />
+            <button className='like' onClick={props.handleLike}>like</button>
+            <button className='block' onClick={props.handleBlock}>block</button>
+            <div className='overview'>{props.overview}</div>
+            
+        </div>
     )
 }
 
