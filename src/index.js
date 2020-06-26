330e0b8ed0e466eebc8807883f1e870e2f601423
lichlp <hao.lip@husky.neu.edu>
2020-06-26 12:12:12 -0400

    new structure

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import MovieList from "./MovieList";
-import NavBar from "./NavBar";
+// import MovieList from "./MovieList";
+import NavBar from "./components/NavBar";
 
 ReactDOM.render(<NavBar />, document.getElementById("root"));
